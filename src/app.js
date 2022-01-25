2b906557a659b8ecf478e24610e226bf07439fe5
arjad <arjadgohar14@gmail.com>
2022-01-25 17:25:59 +0500

    all done

--- src/app.js
+++ src/app.js
@@ -7,32 +7,82 @@ var hbs = require('express-handlebars');
 
 const register = require("./models/registers");
 const async = require("hbs/lib/async");
+const Register = require("./models/registers");
+const { lazyrouter } = require("express/lib/application");
 
 require("./db/conn");
 
 const static_path_of_index = path.join(__dirname, "../public");
+app.use(express.json());//to handle json data
+app.use(express.urlencoded({extended:false}));
 
 app.use( express.static(static_path_of_index));
 app.set("view engine",hbs);
 
 
 app.get("/",(req,res)=>{
-    res.send("home page");
-})
-
-app.get("/register",(req,res)=>{
     res.render("index.hbs");
+})
+app.get("/register",(req,res)=>{
+    res.render("reg.hbs");
 })
 app.post("/register",async(req,res)=>{
     try{
-        console.log(req.body.firstname);
-        req.body.send(req.body.firstname);
+        
+        const pass = req.body.pass;
+        const cpass = req.body.cpass;
+
+        if(pass === cpass)
+        {
+            console.log("passwords matched");
+
+            const regemp = new Register({
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email,
+                pass:req.body.pass,
+                cpass:req.body.cpass,
+                // gender:req.body.gender,
+                // hobbies:req.body.hobbies,
+                // sourceofincome:req.body.sourceofincome,
+                // income:req.body.income,
+                // picurl:req.body.picurl,
+                // age:req.body.age,
+                // bio:req.body.bio        
+            })
+            
+            console.log("reg employ", regemp);
+
+            const reg = await regemp.save();
+            console.log("reg", reg );
+
+            //adding in db
+            var MongoClient = require('mongodb').MongoClient;
+            var url = "mongodb://localhost:27017/";
+
+            MongoClient.connect(url, function(err, db) 
+            {
+              if (err) throw err;
+              var dbo = db.db("mernthapa-db");
+    
+              dbo.collection("mernthapa-col").insertOne(reg, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+              });
+            });
+
+            res.render("index.hbs");
+        }
+        else{
+            res.send("passwords are not matching");
+        }
     }
     catch(e)
     {
-        // res.status(400).send(e);
+        console.log("catch block error", e);
+        res.status(400).send(e);
     }
-
 })
 
 app.listen(port ,()=>{
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//it show error if it is required but not entered
 const emplScehema = new mongoose.Schema({
     firstname:{
         type:String,
@@ -21,36 +22,36 @@ const emplScehema = new mongoose.Schema({
         type:String,
         required:true
     },
-    gender:{
-        type:String,
-        required:true
-    },
-    hobbies:{
-        type:String,
-        required:true
-    },
-    sourceofincome:{
-        type:String,
-        required:true
-    },
-    income:{
-        type:Number,
-        required:true
-    },
-    picurl:{
-        type:String,
-        required:true
-    },
-    age:{
-        type:Number,
-        required:true
-    },
-    bio:{
-        type:String,
-        required:true
-    }
+    // gender:{
+    //     type:String,
+    //     required:true
+    // },
+    // hobbies:{
+    //     type:String,
+    //     required:true
+    // },
+    // sourceofincome:{
+    //     type:String,
+    //     required:true
+    // },
+    // income:{
+    //     type:Number,
+    //     required:true
+    // },
+    // picurl:{
+    //     type:String,
+    //     required:true
+    // },
+    // age:{
+    //     type:Number,
+    //     required:true
+    // },
+    // bio:{
+    //     type:String,
+    //     required:true
+    // }
 })
 
 //now create collection
-const Register = new mongoose.model("mernthapa-col", emplScehema);
+const Register = new mongoose.model("Mernthapa-col", emplScehema);
 module.exports = Register;
